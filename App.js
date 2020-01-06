87a48d6b14025684fd0c5280d4c302e13611fd2e
Sithija Shehara <nsithijashehara@gmail.com>
2020-01-07 00:27:48 +0530

    installed navigation and created com

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>It's my Airbnb clone</Text>
-    </View>
-  );
+//navigation
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+class App extends React.Component{
+  render(){
+    return(
+      <AppContainer/>
+    );
+  }
 }
 
+const AppContainer = createAppContainer(AppTabNavigator);
+const AppTabNavigator = createBottomTabNavigator({
+  Explore:{
+
+  },
+  Svaed:{
+
+  },
+  Trips:{
+
+  },
+  Inbox:{
+    
+  }
+})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
