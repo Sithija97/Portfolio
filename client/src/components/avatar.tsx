33d2120c7a64d805c696ef27ce41bc63e117606d
Sithija Shehara <nsithijashehara@gmail.com>
2024-01-29 17:24:12 +0530

    header Img - issue fix

--- client/src/components/avatar.tsx
+++ client/src/components/avatar.tsx
@@ -1,11 +1,9 @@
-const url =
-  "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D";
-
 type IProps = {
   src?: string;
   styles?: string;
 };
 
-export const Avatar = ({ src = url, styles = "" }: IProps) => {
+export const Avatar = ({ src = "", styles = "" }: IProps) => {
+  console.log(src);
   return <img src={src} alt="profile image" className={styles} />;
 };
